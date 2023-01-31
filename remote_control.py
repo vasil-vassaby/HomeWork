ba3ffdccd3b76d3db41f1dc5f765f84bf936d2b4
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2023-02-01 00:42:53 +0300

    added change solution to file remote_control.py

--- remote_control.py
+++ remote_control.py
@@ -7,38 +7,50 @@ def start():
     while True:
         path = md.set_class(vw.input_class())
         if os.path.exists(path):
-            while True:
-                lesson = md.set_subject(vw.input_subject())
-                md.open_file()
-                lessons = md.get_lesson_list()
-                if lesson in lessons:
-                    while True:
-                        journal = md.get_journal()
-                        vw.list_kids(journal)
-                        student = vw.who_answer()
-                        if student in journal:
-                            while True:
-                                try:
-                                    mark = int(vw.what_mark())
-                                    if 0 < mark < 6:
-                                        md.student_mark(student, mark)
-                                        break
-                                    else:
-                                        print('Укажите оценку от 1 до 5!')
-                                except ValueError:
-                                    print('Укажите целое число!')
-                        elif student == 'Выход':
-                            vw.lesson_over()
-                            break
-                        else:
-                            print(f'Ученика "{student}" нет в журнале! '
-                                  'Введите полностью имя и фамилию школьника '
-                                  'из предложенного ниже списка')
-                    md.save_file()
-                    break
-                else:
-                    print('Укажите полное название предмета из предложенного списка!')
-                    print(lessons)
+            check_subject()
             break
         else:
             print('Укажите существующий класс!')
+
+
+def check_mark(student: str):
+    while True:
+        try:
+            mark = int(vw.what_mark())
+            if 0 < mark < 6:
+                md.student_mark(student, mark)
+                break
+            else:
+                print('Укажите оценку от 1 до 5!')
+        except ValueError:
+            print('Укажите целое число!')
+
+
+def check_student():
+    while True:
+        journal = md.get_journal()
+        vw.list_kids(journal)
+        student = vw.who_answer()
+        if student in journal:
+            check_mark(student)
+        elif student == 'Выход':
+            vw.lesson_over()
+            break
+        else:
+            print(f'Ученика "{student}" нет в журнале! '
+                  'Введите полностью имя и фамилию школьника '
+                  'из предложенного ниже списка')
+
+
+def check_subject():
+    while True:
+        lesson = md.set_subject(vw.input_subject())
+        md.open_file()
+        lessons = md.get_lesson_list()
+        if lesson in lessons:
+            check_student()
+            md.save_file()
+            break
+        else:
+            print('Укажите полное название предмета из предложенного списка!')
+            print(lessons)
